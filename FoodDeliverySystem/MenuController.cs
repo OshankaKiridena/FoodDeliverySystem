c778f4b740429b33691f21c9fda25128f8c54f21
Vihagadesilva <vihangadesilva82@gmail.com>
2025-06-13 18:19:44 +0530

    menu items added

---
+++ FoodDeliverySystem/MenuController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDeliverySystem
+{
+    public class MenuController
+    {
+        private List<MenuItem> menuItems = new List<MenuItem>();
+
+        public void AddMenuItem(MenuItem item) => menuItems.Add(item);
+        public List<MenuItem> GetMenuItems() => menuItems;
+        public void UpdateMenuItem(MenuItem updatedItem)
+        {
+            var item = menuItems.FirstOrDefault(m => m.Id == updatedItem.Id);
+            if (item != null)
+            {
+                item.Name = updatedItem.Name;
+                item.Description = updatedItem.Description;
+                item.Price = updatedItem.Price;
+                item.Category = updatedItem.Category;
+            }
+        }
+        public void DeleteMenuItem(int id) =>
+            menuItems.RemoveAll(m => m.Id == id);
+    }
+
+}
